7704de4e219d90e3f5d782bbdd2667406c1ebd12
ingchrist10 <ingchrist10@gmail.com>
2025-06-16 01:35:33 +0100

    in the mist of doing hard things by ingchrist: file/folder change detected

--- backend-codes/core/settings.py
+++ backend-codes/core/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "your_database_name",
+        "USER": "your_database_user",
+        "PASSWORD": "your_database_password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
