88692745fb1c6bc796ebbfca097faab9364cd073
ingchrist <cdjinguet@gmail.com>
2025-07-29 23:27:44 +0000

    ok

--- apps/courses/urls.py
+++
@@ -1,9 +0,0 @@
-# apps/courses/urls.py
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # Add your URL patterns here
-    # Example: path('your-url/', views.your_view, name='url-name'),
-]
